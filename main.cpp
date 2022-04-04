98d41acab7d58f2460ec3843426603d72406f046
ahmadrifqi19 <101203261+ahmadrifqi19@users.noreply.github.com>
2022-04-04 14:50:17 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+//Nama: Ahmad Rifqi Fauzi
+//NIM: 1121031013
+//Kelas: A
+//Nama File main.cpp
+/*Deskripsi :　Memasukan jam, menit, dan detik lalu di validasi dengan fungsi isValid,
+Mengembalikan nilai true jika memenuhi ketentuan.*/
+
+int main()
+{
+    int HH,MM,SS,a,b;
+    b=a=0;
+    while(b==0)
+    {
+    cout<<"set jam : "<<endl;
+    cin>>HH;
+    cout<<"set menit : "<<endl;
+    cin>>MM;
+    cout<<"set detik: "<<endl;
+    cin>>SS;
+    if (HH<<23 && MM << 59 && SS <<59)
+    b++;
+    else
+        system ("cls");
+    }
+    while (a==0)
+        {
+        system ("cls");
+        cout << HH << ":" << MM << ":" << SS << endl;
+        Sleep(1000);
+        SS++;
+        if (SS>59)
+        {
+            SS=0;
+       MM++;
+        }
+
+        if (MM > 59)
+        {
+            MM=0;
+        HH++;
+        }
+        if (HH > 23)
+        {
+            HH=0;
+        }
+        }
+        return 0;
+}
